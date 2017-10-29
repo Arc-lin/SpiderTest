4feb2b933d8312eb4bafb9f52c3ab3d54e62acf9
Arclin <arclin@dankal.cn>
2017-10-30 01:23:53 +0800

    测试

--- routes/jiandan.js
+++ routes/jiandan.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/test', function(req, res, next) {
+  res.send('test2');
+});
+
 module.exports = router;
